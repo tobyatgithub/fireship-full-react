45cc7954d87945e343b7a1b63938b150a0ea06f4
tobyAtMakersights <toby@makersights.com>
2022-04-13 14:41:06 -0700

    add exercise for setting current count and prv count by one onclick event. interestingly, the onclick requires a const arrow function. and will yield many errors if pass in a function directly.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// import "./style.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [previousCount, setPreviousCount] = useState(0);
+
+  const handleClick = () => {
+    setPreviousCount(count);
+    setCount(count + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Current Count: {count}</h3>
+      <h3>Previous Count: {previousCount}</h3>
+      <button onClick={handleClick}>+</button>
     </div>
   );
 }
